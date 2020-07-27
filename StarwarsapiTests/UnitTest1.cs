ad5cea0cdba6c6cff42c4d4c21605c3b75c9c4c9
Danielly Araujo <araaujodann@gmail.com>
2020-07-27 15:09:15 -0300

    Add project files.

---
+++ StarwarsapiTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace StarwarsapiTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
